dd5203bdb8691b0011e9ef184f4869ce342f1517
MutoAyumu <mutoayum0610@gmail.com>
2022-10-18 12:48:38 +0900

    [update]UIの配置

--- Assets/Scripts/WeaponButton.cs
+++ Assets/Scripts/WeaponButton.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class WeaponButton : MonoBehaviour
 {
     [SerializeField] Text _text;
+    [SerializeField]Image _image;
 
     int _id;
     WeaponType _type;
@@ -22,6 +23,26 @@ public class WeaponButton : MonoBehaviour
         _id = data.ID;
         _type = data.Type;
         _text.text = data.Name;
+
+        SetTypeIcon(_type);
+    }
+
+    void SetTypeIcon(WeaponType type)
+    {
+        Sprite sprite = default;
+
+        //ResourcesÇ©ÇÁÉAÉCÉRÉìÇì«Ç›çûÇﬁ
+        switch(type)
+        {
+            case WeaponType.AR:
+                sprite = Resources.Load<Sprite>("WeaponIcon/ARIcon");
+                break;
+            case WeaponType.SMG:
+                sprite = Resources.Load<Sprite>("WeaponIcon/SMGIcon");
+                break;
+        }
+
+        _image.sprite = sprite;
     }
 
     /// <summary>
